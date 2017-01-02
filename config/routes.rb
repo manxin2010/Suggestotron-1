1dc15f654a9fea3b75dff80790146a6a96c13c50
manxin2010 <manxin2010@163.com>
2017-01-02 12:22:54 +0800

    first rails project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'topics#index'
+ resources :topics do
+   member do
+     post 'upvote'
+   end
+ end
 end
